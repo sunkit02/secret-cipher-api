25f3c00f8c325be0f5fd4c54c6dce31c44de1b6f
Sun Kit Tsui <sunkittsui@gmail.com>
2022-12-28 16:58:07 -0500

    Implemented endpoint for users to fetch SentMessages

--- src/main/java/com/sunkit/secretcipher/controllers/UserController.java
+++ src/main/java/com/sunkit/secretcipher/controllers/UserController.java
@@ -1,12 +1,15 @@
 package com.sunkit.secretcipher.controllers;
 
 import com.sunkit.secretcipher.expections.UserNotFoundException;
-import com.sunkit.secretcipher.models.user.User;
-import com.sunkit.secretcipher.repos.UserRepository;
+import com.sunkit.secretcipher.models.message.MessageDTO;
+import com.sunkit.secretcipher.models.payloads.requests.GetSentMessagesRequest;
+import com.sunkit.secretcipher.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @CrossOrigin(originPatterns = "*")
@@ -15,15 +18,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/user")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable Long userId) throws UserNotFoundException {
-        return ResponseEntity.ok(
-                userRepository.findById(userId)
-                        .orElseThrow(() -> new UserNotFoundException(
-                                "User with user id: " + userId + " not found"))
-        );
+    @PostMapping("/sent-messages")
+    public ResponseEntity<?> getSentMessages(
+            @RequestBody GetSentMessagesRequest request) {
+        List<MessageDTO> sentMessages;
+        try {
+            sentMessages = userService.getSentMessages(request.username());
+
+        } catch (UserNotFoundException e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
+
+        return ResponseEntity.ok(sentMessages);
     }
 
 }
--- src/main/java/com/sunkit/secretcipher/services/UserService.java
+++ src/main/java/com/sunkit/secretcipher/services/UserService.java
@@ -1,20 +1,35 @@
 package com.sunkit.secretcipher.services;
 
 import com.sunkit.secretcipher.expections.UserNotFoundException;
+import com.sunkit.secretcipher.models.message.MessageDTO;
 import com.sunkit.secretcipher.models.user.User;
+import com.sunkit.secretcipher.repos.MessageRepository;
 import com.sunkit.secretcipher.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final MessageRepository messageRepository;
 
     public User findByUsername(String username) throws UserNotFoundException {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UserNotFoundException(String.format(
                         "User with username '%s' not found", username)));
+    }
+
+    public List<MessageDTO> getSentMessages(String username) throws UserNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(String.format(
+                        "User with username '%s' not found", username)));
+        return messageRepository.findAllBySenderOrderByTimeSentDesc(user)
+                .stream()
+                .map(MessageDTO::of)
+                .toList();
     }
 }
